7fc53c049b2c629253602a9855b450bbd030c650
Sojin Park <luv2hong@gmail.com>
2023-11-07 00:00:29 +0900

    [Add] AppDetailVC 추가

---
+++ RxSwift_iTunes/Scene/AppDetail/AppDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AppDetailViewController.swift
+//  RxSwift_iTunes
+//
+//  Created by 박소진 on 2023/11/06.
+//
+
+import UIKit
+
+final class AppDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+    
+}
