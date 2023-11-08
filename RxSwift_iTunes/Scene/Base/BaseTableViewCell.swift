99424db8d243c1f5ae6c11107483f97dbe477919
Sojin Park <luv2hong@gmail.com>
2023-11-08 20:43:44 +0900

    [Add] BaseTableViewCell 추가

---
+++ RxSwift_iTunes/Scene/Base/BaseTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BaseTableViewCell.swift
+//  RxSwift_iTunes
+//
+//  Created by 박소진 on 2023/11/08.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+        setConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {}
+    
+    func setConstraints() {}
+}
