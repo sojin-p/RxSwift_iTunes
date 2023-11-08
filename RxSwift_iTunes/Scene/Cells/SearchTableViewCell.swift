791ba812e1dcd00099a3f2df3c466070ba08777c
Sojin Park <luv2hong@gmail.com>
2023-11-08 20:45:54 +0900

    [Style] SearchTableViewCell AppName관련 CustomView로 변경

--- RxSwift_iTunes/Scene/Cells/SearchTableViewCell.swift
+++ RxSwift_iTunes/Scene/Cells/SearchTableViewCell.swift
@@ -7,36 +7,11 @@
 
 import UIKit
 
-final class SearchTableViewCell: UITableViewCell {
+final class SearchTableViewCell: BaseTableViewCell {
     
     static let identifier = "SearchTableViewCell"
     
-    let appNameLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 17, weight: .light)
-        label.textColor = .black
-        return label
-    }()
-    
-    let appIconImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 10
-        imageView.layer.borderWidth = 1
-        imageView.layer.borderColor = UIColor.systemGray5.cgColor
-        return imageView
-    }()
-    
-    let downloadButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("받기", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
-        button.isUserInteractionEnabled = true
-        button.backgroundColor = .systemGray5
-        button.layer.cornerRadius = 16
-        return button
-    }()
+    let appNameView = AppNameView()
     
     let labelStackView = {
         let stackView = UIStackView()
@@ -93,24 +68,14 @@ final class SearchTableViewCell: UITableViewCell {
     let screenshotImageview2 = UIImageView()
     let screenshotImageview3 = UIImageView()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    func configureCell(element: AppInfo) {
         
         self.selectionStyle = .none
         
-        configure()
-        setConstraints()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func configureCell(element: AppInfo) {
-        appNameLabel.text = element.trackName
+        appNameView.appNameLabel.text = element.trackName
         
         let url = URL(string: element.artworkUrl512)
-        appIconImageView.kf.setImage(with: url)
+        appNameView.appIconImageView.kf.setImage(with: url)
         
         let screenshotUrls = element.screenshotUrls
         let test = [screenshotImageview1, screenshotImageview2, screenshotImageview3]
@@ -127,8 +92,8 @@ final class SearchTableViewCell: UITableViewCell {
         
     }
     
-    private func configure() {
-        [appNameLabel, appIconImageView, downloadButton, labelStackView, screenStackView].forEach { contentView.addSubview($0) }
+    override func configure() {
+        [appNameView.appNameLabel, appNameView.appIconImageView, appNameView.downloadButton, labelStackView, screenStackView].forEach { contentView.addSubview($0) }
         
         [rateStackView, sellerNameLabel, genreLabel].forEach { labelStackView.addArrangedSubview($0) }
         
@@ -157,29 +122,29 @@ final class SearchTableViewCell: UITableViewCell {
         
     }
     
-    private func setConstraints() {
+    override func setConstraints() {
         
-        appIconImageView.snp.makeConstraints {
+        appNameView.appIconImageView.snp.makeConstraints {
             $0.top.equalToSuperview().offset(10)
             $0.leading.equalTo(20)
             $0.size.equalTo(60)
         }
         
-        appNameLabel.snp.makeConstraints {
-            $0.centerY.equalTo(appIconImageView)
-            $0.leading.equalTo(appIconImageView.snp.trailing).offset(10)
-            $0.trailing.equalTo(downloadButton.snp.leading).offset(-10)
+        appNameView.appNameLabel.snp.makeConstraints {
+            $0.centerY.equalTo(appNameView.appIconImageView)
+            $0.leading.equalTo(appNameView.appIconImageView.snp.trailing).offset(10)
+            $0.trailing.equalTo(appNameView.downloadButton.snp.leading).offset(-10)
         }
         
-        downloadButton.snp.makeConstraints {
-            $0.centerY.equalTo(appIconImageView)
+        appNameView.downloadButton.snp.makeConstraints {
+            $0.centerY.equalTo(appNameView.appIconImageView)
             $0.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(32)
             $0.width.equalTo(72)
         }
         
         labelStackView.snp.makeConstraints {
-            $0.top.equalTo(appIconImageView.snp.bottom).offset(8)
+            $0.top.equalTo(appNameView.appIconImageView.snp.bottom).offset(8)
             $0.horizontalEdges.equalToSuperview().inset(23)
             $0.height.equalTo(23)
         }
@@ -190,7 +155,7 @@ final class SearchTableViewCell: UITableViewCell {
         
         screenStackView.snp.makeConstraints {
             $0.top.equalTo(labelStackView.snp.bottom).offset(8)
-            $0.height.equalTo(appIconImageView.snp.height).multipliedBy(3.2)
+            $0.height.equalTo(appNameView.appIconImageView.snp.height).multipliedBy(3.2)
             $0.horizontalEdges.equalToSuperview().inset(20)
             $0.bottom.equalToSuperview().offset(-10)
         }
